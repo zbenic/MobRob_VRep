0d38ca5fb22ffdd4a0c18b48684927e13224a624
Jarvis Pennyworth <zbenic47250@gmail.com>
2019-03-20 23:11:16 +0100

    Remote API connection verification

--- main.py
+++ main.py
@@ -9,6 +9,9 @@
 # IMPORTANT: for each successful call to simxStart, there
 # should be a corresponding call to simxFinish at the end!
 
+# edit usrset.txt to turn on debugging
+# debug log file debugLog.txt
+
 import sys
 import subprocess
 import time
@@ -24,37 +27,45 @@ except:
     print('--------------------------------------------------------------')
     print('')
 
-subprocess.call("C:/Program Files/V-REP3/V-REP_PRO_EDU/vrep.exe -gREMOTEAPISERVERSERVICE_19997_TRUE_TRUE G:/GIT/MobRob/Scene/labScene.ttt")
-time.sleep(5)
+subprocess.Popen(['C:/Program Files/V-REP3/V-REP_PRO_EDU/vrep.exe', '-gREMOTEAPISERVERSERVICE_19997_FALSE_TRUE', 'G:/GIT/MobRob/Scene/labScene.ttt'])
+time.sleep(7)
 
 print('Program started')
 vrep.simxFinish(-1)  # just in case, close all opened connections
-clientID = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  # Connect to V-REP
+clientId = vrep.simxStart('127.0.0.1', 19997, True, True, 5000, 5)  # Connect to V-REP
 
-if clientID != -1:
+if clientId != -1:
     print('Connected to remote API server')
 else:
     print('Failed connecting to remote API server')
     sys.exit('Could not connect')
 
-vrep.simxStartSimulation(clientID, vrep.simx_opmode_oneshot)
-
 startTime = time.time()
 
-errorCode, leftMotor = vrep.simxGetObjectHandle(clientID, "leftMotor", vrep.simx_opmode_oneshot_wait)
-errorCode, rightMotor = vrep.simxGetObjectHandle(clientID, "rightMotor", vrep.simx_opmode_oneshot_wait)
+errorCodeLeft, leftMotor = vrep.simxGetObjectHandle(clientId, "leftMotor", vrep.simx_opmode_oneshot_wait)
+errorCodeRight, rightMotor = vrep.simxGetObjectHandle(clientId, "rightMotor", vrep.simx_opmode_oneshot_wait)
 
-vrep.simxSetJointTargetVelocity(clientID, leftMotor, 0.2, vrep.simx_opmode_oneshot)
+vrep.simxSynchronous(clientId, 1) # enable the synchronous mode (client side). The server side (i.e. V-REP) also needs to be enabled.
+vrep.simxStartSimulation(clientId, vrep.simx_opmode_oneshot) # start the simulation
+vrep.simxSetJointForce(clientId, leftMotor, 1.0, vrep.simx_opmode_oneshot) # set the joint force/torque
+vrep.simxSetJointForce(clientId, rightMotor, 1.0, vrep.simx_opmode_oneshot) # set the joint force/torque
 
-time.sleep(2)
+simStep = 0
+while(simStep < 100):
+    vrep.simxSetJointTargetVelocity(clientId, leftMotor, 5, vrep.simx_opmode_oneshot) # set the joint target velocity
+    vrep.simxSetJointTargetVelocity(clientId, rightMotor, 5, vrep.simx_opmode_oneshot) # set the joint target velocity
+    vrep.simxSynchronousTrigger(clientId) # trigger next simulation step. Above commands will be applied
+    simStep += 1
+
+vrep.simxPauseSimulation(clientId, vrep.simx_opmode_oneshot) # stop the simulation
 
 
 # Before closing the connection to V-REP, make sure that the last command sent out had time to arrive.
 # You can guarantee this with (for example):
-vrep.simxGetPingTime(clientID)
+vrep.simxGetPingTime(clientId)
 
 # Now close the connection to V-REP:
-vrep.simxFinish(clientID)
+vrep.simxFinish(clientId)
 
 
 print ('Program ended')

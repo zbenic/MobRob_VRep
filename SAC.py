29bf53fb53e61a943f34c43a4399e20b199604e4
Zoran Benic <zoran.benic.external@veoneer.com>
2019-08-17 09:48:34 +0200

    New algo tryout

---
+++ SAC.py
@@ -0,0 +1,219 @@
+from collections import namedtuple
+
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Normal
+from tensorboardX import SummaryWriter
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+
+Transition = namedtuple('Transition', ['s', 'a', 'r', 's_', 'd'])
+min_Val = torch.tensor(1e-7).float().to(device)
+
+class Actor(nn.Module):
+    def __init__(self, state_dim, max_action, min_log_std=-10, max_log_std=2):
+        super(Actor, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.mu_head = nn.Linear(256, 1)
+        self.log_std_head = nn.Linear(256, 1)
+        self.max_action = max_action
+
+        self.min_log_std = min_log_std
+        self.max_log_std = max_log_std
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        mu = self.mu_head(x)
+        log_std_head = F.relu(self.log_std_head(x))
+        log_std_head = torch.clamp(log_std_head, self.min_log_std, self.max_log_std)
+        return mu, log_std_head
+
+
+class Critic(nn.Module):
+    def __init__(self, state_dim):
+        super(Critic, self).__init__()
+        self.fc1 = nn.Linear(state_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class Q(nn.Module):
+    def __init__(self, state_dim, action_dim):
+        super(Q, self).__init__()
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.fc1 = nn.Linear(state_dim + action_dim, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 1)
+
+    def forward(self, s, a):
+        s = s.reshape(-1, self.state_dim)
+        a = a.reshape(-1, self.action_dim)
+        x = torch.cat((s, a), -1)  # combination s and a
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class SAC():
+    def __init__(self, lr, state_dim, action_dim, max_action, capacity=10000,
+                 gradient_steps=1, batch_size=128, gamma=0.99, tau=0.005):
+        super(SAC, self).__init__()
+
+        self.capacity = capacity
+        self.gradient_steps = gradient_steps
+        self.batch_size = batch_size
+        self.gamma = gamma
+        self.tau = tau
+
+        self.policy_net = Actor(state_dim, max_action).to(device)
+        self.value_net = Critic(state_dim).to(device)
+        self.Target_value_net = Critic(state_dim).to(device)
+        self.Q_net1 = Q(state_dim, action_dim).to(device)
+        self.Q_net2 = Q(state_dim, action_dim).to(device)
+
+        self.policy_optimizer = optim.Adam(self.policy_net.parameters(), lr=lr)
+        self.value_optimizer = optim.Adam(self.value_net.parameters(), lr=lr)
+        self.Q1_optimizer = optim.Adam(self.Q_net1.parameters(), lr=lr)
+        self.Q2_optimizer = optim.Adam(self.Q_net2.parameters(), lr=lr)
+
+        self.replay_buffer = [Transition] * self.capacity
+        self.num_transition = 0 # pointer of replay buffer
+        self.num_training = 1
+        self.writer = SummaryWriter('./exp-SAC_dual_Q_network')
+
+        self.value_criterion = nn.MSELoss()
+        self.Q1_criterion = nn.MSELoss()
+        self.Q2_criterion = nn.MSELoss()
+
+        for target_param, param in zip(self.Target_value_net.parameters(), self.value_net.parameters()):
+            target_param.data.copy_(param.data)
+
+        os.makedirs('./SAC_model/', exist_ok=True)
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state).to(device)
+        mu, log_sigma = self.policy_net(state)
+        sigma = torch.exp(log_sigma)
+        dist = Normal(mu, sigma)
+        z = dist.sample()
+        action = torch.tanh(z).detach().cpu().numpy()
+        return action.item() # return a scalar, float32
+
+    def store(self, s, a, r, s_, d):
+        index = self.num_transition % self.capacity
+        transition = Transition(s, a, r, s_, d)
+        self.replay_buffer[index] = transition
+        self.num_transition += 1
+
+    def evaluate(self, state):
+        batch_mu, batch_log_sigma = self.policy_net(state)
+        batch_sigma = torch.exp(batch_log_sigma)
+        dist = Normal(batch_mu, batch_sigma)
+        noise = Normal(0, 1)
+
+        z = noise.sample()
+        action = torch.tanh(batch_mu + batch_sigma*z.to(device))
+        log_prob = dist.log_prob(batch_mu + batch_sigma * z.to(device)) - torch.log(1 - action.pow(2) + min_Val)
+        return action, log_prob, z, batch_mu, batch_log_sigma
+
+    def update(self):
+        if self.num_training % 500 == 0:
+            print("Training ... {} times ".format(self.num_training))
+        s = torch.tensor([t.s for t in self.replay_buffer]).float().to(device)
+        a = torch.tensor([t.a for t in self.replay_buffer]).float().to(device)
+        r = torch.tensor([t.r for t in self.replay_buffer]).float().to(device)
+        s_ = torch.tensor([t.s_ for t in self.replay_buffer]).float().to(device)
+        d = torch.tensor([t.d for t in self.replay_buffer]).float().to(device)
+
+        for _ in range(self.gradient_steps):
+            #for index in BatchSampler(SubsetRandomSampler(range(args.capacity)), args.batch_size, False):
+            index = np.random.choice(range(self.capacity), self.batch_size, replace=False)
+            bn_s = s[index]
+            bn_a = a[index].reshape(-1, 1)
+            bn_r = r[index].reshape(-1, 1)
+            bn_s_ = s_[index]
+            bn_d = d[index].reshape(-1, 1)
+
+            target_value = self.Target_value_net(bn_s_)
+            next_q_value = bn_r + (1 - bn_d) * self.gamma * target_value
+
+            excepted_value = self.value_net(bn_s)
+            excepted_Q1 = self.Q_net1(bn_s, bn_a)
+            excepted_Q2 = self.Q_net2(bn_s, bn_a)
+            sample_action, log_prob, z, batch_mu, batch_log_sigma = self.evaluate(bn_s)
+            excepted_new_Q = torch.min(self.Q_net1(bn_s, sample_action), self.Q_net2(bn_s, sample_action))
+            next_value = excepted_new_Q - log_prob
+
+            # !!!Note that the actions are sampled according to the current policy,
+            # instead of replay buffer. (From original paper)
+            V_loss = self.value_criterion(excepted_value, next_value.detach()).mean()  # J_V
+
+            # Dual Q net
+            Q1_loss = self.Q1_criterion(excepted_Q1, next_q_value.detach()).mean() # J_Q
+            Q2_loss = self.Q2_criterion(excepted_Q2, next_q_value.detach()).mean()
+
+            pi_loss = (log_prob - excepted_new_Q).mean() # according to original paper
+
+            self.writer.add_scalar('Loss/V_loss', V_loss, global_step=self.num_training)
+            self.writer.add_scalar('Loss/Q1_loss', Q1_loss, global_step=self.num_training)
+            self.writer.add_scalar('Loss/Q2_loss', Q2_loss, global_step=self.num_training)
+            self.writer.add_scalar('Loss/policy_loss', pi_loss, global_step=self.num_training)
+
+            # mini batch gradient descent
+            self.value_optimizer.zero_grad()
+            V_loss.backward(retain_graph=True)
+            nn.utils.clip_grad_norm_(self.value_net.parameters(), 0.5)
+            self.value_optimizer.step()
+
+            self.Q1_optimizer.zero_grad()
+            Q1_loss.backward(retain_graph = True)
+            nn.utils.clip_grad_norm_(self.Q_net1.parameters(), 0.5)
+            self.Q1_optimizer.step()
+
+            self.Q2_optimizer.zero_grad()
+            Q2_loss.backward(retain_graph = True)
+            nn.utils.clip_grad_norm_(self.Q_net2.parameters(), 0.5)
+            self.Q2_optimizer.step()
+
+            self.policy_optimizer.zero_grad()
+            pi_loss.backward(retain_graph = True)
+            nn.utils.clip_grad_norm_(self.policy_net.parameters(), 0.5)
+            self.policy_optimizer.step()
+
+            # update target v net update
+            for target_param, param in zip(self.Target_value_net.parameters(), self.value_net.parameters()):
+                target_param.data.copy_(target_param * (1 - self.tau) + param * self.tau)
+
+            self.num_training += 1
+
+    def save(self):
+        torch.save(self.policy_net.state_dict(), './SAC_model/policy_net.pth')
+        torch.save(self.value_net.state_dict(), './SAC_model/value_net.pth')
+        torch.save(self.Q_net1.state_dict(), './SAC_model/Q_net1.pth')
+        torch.save(self.Q_net2.state_dict(), './SAC_model/Q_net2.pth')
+        print("====================================")
+        print("Model has been saved...")
+        print("====================================")
+
+    def load(self):
+        self.policy_net.load_state_dict(torch.load('./SAC_model/policy_net.pth'))
+        self.value_net.load_state_dict(torch.load( './SAC_model/value_net.pth'))
+        self.Q_net1.load_state_dict(torch.load('./SAC_model/Q_net1.pth'))
+        self.Q_net2.load_state_dict(torch.load('./SAC_model/Q_net2.pth'))
+        print("model has been load")
---
+++ tryout_gymEnv_SAC.py
@@ -0,0 +1,208 @@
+import torch
+import numpy as np
+import sys, os
+
+sys.path.append("C:/visageGIT\TD3-PyTorch-BipedalWalker-v2")
+from SAC import SAC
+from utils import ReplayBuffer, EnvironmentTiles
+
+import time
+import matplotlib.pyplot as plt
+
+from labEnv import LabEnv, MobRob
+import gc
+
+import gym
+
+
+def main():
+    train = True
+    vrepHeadlessMode = False
+    simulate = True
+    plot = True
+
+    envLength = 2.5
+    envWidth = 2.0
+    envOriginCoordinate = [-0.5, -1.0]  # double T maze
+    tileSize = [0.1, 0.1]
+
+    ######### Hyperparameters #########
+    # env_name = "MobRob-Corridor"
+    # env_name = "MobRob-U_maze-working"
+    env_name = "MountainCarContinuous-v0"
+    # env_name = "Pendulum-v0"
+
+    log_interval = 10           # print avg reward after interval
+    random_seed = 0
+    gamma = 0.99                # discount for future rewards
+    min_batch_size = 100
+    batch_size = 100            # num of transitions sampled from replay buffer
+    lr = 0.0003
+    initial_exploration_noise = 0.1     # was 0.1
+    exploration_noise = 0.1
+    polyak = 0.995              # target policy update parameter (1-tau)
+    policy_noise = 0.2          # target policy smoothing noise
+    noise_clip = 0.5
+    policy_delay = 2            # delayed policy updates parameter
+    episodes = 1000             # max num of episodes
+    steps = 2000                 # max timesteps in one episode
+    iterations_per_step = 1
+    directory = "./preTrained/{}".format(env_name)  # save trained models
+    filename = "TD3_{}_{}".format(env_name, random_seed)
+    ###################################
+
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+
+
+    if train:
+        env = gym.make(env_name)
+
+        # policy = TD3(lr, env.observation_space.shape[0], env.action_space.shape[0], env.max_action)
+        policy = SAC(lr, env.observation_space.shape[0], env.action_space.shape[0], env.action_space.high[0])
+
+
+        if policy is not None:
+            print('SAC agent initialized')
+        else:
+            print('SAC agent failed to initialize')
+            exit(-1)
+
+        # logging variables:
+        log_f = open("log.txt", "w+")
+
+        total_num_of_steps = 0
+        total_rewards = []
+        save_rewards = []
+        durations = []
+        avg_reward = 0
+
+        for episode in range(episodes):
+            start_time = time.time()
+            state = env.reset()
+            episode_rewards = []
+            log_f.write('Episode {}\n'.format(episode))
+            for step in range(steps+1):
+                env.render()
+                total_num_of_steps += 1
+
+                # select action and add exploration noise:
+                action = policy.select_action(state)
+                action = action + np.random.normal(0, exploration_noise, size=env.action_space.shape[0])
+                # action = action.clip(env.min_action, env.max_action)
+                action = action.clip(env.action_space.low[0], env.action_space.high[0])
+
+                # take action in env:
+                next_state, reward, done, info = env.step(action)
+                policy.store(state, action, reward, next_state, done)
+
+                if policy.num_transition >= policy.capacity:
+                    policy.update()
+
+                state = next_state
+                episode_rewards.append(reward)
+
+                if done or step == steps:
+                    break
+
+            episode_reward = np.sum(episode_rewards)
+            min_score = np.min(episode_rewards)
+            max_score = np.max(episode_rewards)
+            total_rewards.append(episode_reward)
+            duration = time.time() - start_time
+            durations.append(duration)
+            save_rewards.append([episode_reward, episode])
+
+            eta = np.mean(durations)*(episodes-episode) / 60 / 60
+            if eta < 1.0:
+                etaString = str(np.round(eta * 60, 2)) + " min"
+            else:
+                etaString = str(np.round(eta, 2)) + " h"
+
+            # logging updates:
+            log_f.write('  Episode reward: {}\n'.format(episode_reward))
+            log_f.flush()
+
+            if len(total_rewards) >= log_interval:
+                avg_reward = np.mean(total_rewards[-log_interval:])
+
+            print(
+                '\rEpisode {}\tEpisode reward: {:.2f}\tMin: {:.2f}\tMax: {:.2f}\tAvg_reward: {:.2f}\tDuration: {:.2f}\tETA: {}'
+                    .format(episode, episode_reward, min_score, max_score, avg_reward, duration, etaString))
+
+            gc.collect()
+
+            # if average reward > 300 then save and stop traning:
+            if len(total_rewards) >= log_interval:
+                if avg_reward >= 1100:
+                    print("########## Solved! ###########")
+                    name = filename + '_solved'
+                    policy.save(directory, name)
+                    replay_buffer.save()
+                    log_f.close()
+                    break
+
+            if episode > 500 and episode % log_interval == 0:
+                policy.save(directory, filename)
+                replay_buffer.save()
+
+        np.save('mean_episode_rewards', save_rewards)
+    elif simulate:
+        random_seed = 0
+        n_episodes = 3
+        lr = 0.002
+        max_timesteps = 2000
+
+        filename = "TD3_{}_{}".format(env_name, random_seed)
+        # filename += '_solved'
+        directory = "./preTrained/{}".format(env_name)
+
+        environmentTiles = EnvironmentTiles(envWidth, envLength, envOriginCoordinate, tileSize)
+        mobRob = MobRob(['MobRob'],
+                        ['leftMotor', 'rightMotor'],
+                        ['proximitySensor0', 'proximitySensor1', 'proximitySensor2', 'proximitySensor3', 'proximitySensor4',
+                         'proximitySensor5'])
+        env = LabEnv(mobRob, terminalState, actionBounds, environmentTiles, vrepHeadlessMode)
+        policy = TD3(lr, state_dim, action_dim, max_action)
+
+        if policy is not None:
+            print('TD3 agent initialized')
+        else:
+            print('TD3 agent failed to initialize')
+            exit(-1)
+
+        policy.load_actor(directory, filename)
+
+        for episode in range(1, n_episodes + 1):
+            ep_reward = 0
+            _, state = env.reset()
+            for step in range(max_timesteps):
+                action = policy.select_action(state)
+                state, reward, done, _ = env.step(action)
+                ep_reward += reward
+
+                if done:
+                    break
+
+            print('Episode: {}\tReward: {}'.format(episode, int(ep_reward)))
+            ep_reward = 0
+        env.stop()
+
+    elif plot:
+        mean_episode_rewards = np.load('./mean_episode_rewards.npy')
+
+        f, ax = plt.subplots(1, sharex=True, sharey=False)
+
+        # # Always
+        ax.set_ylabel("Episode reward")
+        ax.set_xlabel("Number of episodes")
+
+        # Highlight the starting x axis
+        ax.axhline(0, color="#AAAAAA")
+        ax.plot(mean_episode_rewards[:, 1], mean_episode_rewards[:, 0])
+        ax.grid(True)
+
+        plt.show()
+
+if __name__ == "__main__":
+    main()
